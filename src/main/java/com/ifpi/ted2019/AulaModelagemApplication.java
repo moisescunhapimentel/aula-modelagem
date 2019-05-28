c8fdb9fc94088915daadfa45548d7509c79152f6
Moisés Cunha Pimentel <2017118050260@ifpi.int>
2019-05-28 17:20:33 -0300

    Primeiro commit

---
+++ src/main/java/com/ifpi/ted2019/AulaModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.ifpi.ted2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaModelagemApplication.class, args);
+	}
+
+}
