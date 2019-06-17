025001d3723bfa7671299ae93045c667bab80868
Moisés Cunha Pimentel <2017118050260@ifpi.int>
2019-06-17 08:32:34 -0300

    Primeira classe de domínio

--- src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
+++ src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.ifpi.ted2019.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpi.ted2019.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(new Categoria(1, "Informática"));
+		categorias.add(new Categoria(2, "Escritório"));
+		return categorias;
 	}
+//	public String lista() {
+//		return "Rest funcionando";
+//	}
 }
