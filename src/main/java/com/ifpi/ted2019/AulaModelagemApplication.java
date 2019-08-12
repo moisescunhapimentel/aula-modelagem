be884fefd68f264c954b209569c867e7df94ad45
kauanYuri <kauanspfc16@gmail.com>
2019-08-12 09:09:12 -0300

    Inicializando valores no banco de dados

--- src/main/java/com/ifpi/ted2019/AulaModelagemApplication.java
+++ src/main/java/com/ifpi/ted2019/AulaModelagemApplication.java
@@ -1,13 +1,32 @@
 package com.ifpi.ted2019;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulaModelagemApplication {
+import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AulaModelagemApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria categoria = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria, categoria2));
 	}
 
 }
