4ba9c69f4961acaec91dc060a3fb766a812c0872
moisescunhapimentel <moisescunhapimentel@gmail.com>
2019-09-02 17:26:38 -0300

    Ajustes no endpoint categorias/{id}

--- src/main/java/com/ifpi/ted2019/services/CategoriaService.java
+++ src/main/java/com/ifpi/ted2019/services/CategoriaService.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Service;
 
 import com.ifpi.ted2019.domain.Categoria;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
-		return repository.findById(id).orElse(null);
+		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
--- src/main/java/com/ifpi/ted2019/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/ifpi/ted2019/services/exceptions/ObjectNotFoundException.java
@@ -1,5 +1,19 @@
 package com.ifpi.ted2019.services.exceptions;
 
-public class ObjectNotFoundException {
+import java.io.Serializable;
 
+public class ObjectNotFoundException extends RuntimeException implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 }
